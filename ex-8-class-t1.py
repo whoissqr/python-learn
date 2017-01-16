d559a1c16c4906a12ff4c0001fda6646351c5ea2
sqr <kk.shichao@gmail.com>
2017-01-16 23:14:18 +0800

    Update ex-8-class-t1.py

--- ex-8-class-t1.py
+++ ex-8-class-t1.py
@@ -9,10 +9,22 @@ class Student(object):
     def print_score(self):
         print ("{}: {}".format(self.name, self.score))
 
+    @classmethod
+    def print_cls(cls):
+        print ("{}".format(cls))
 
+    @staticmethod
+    def print_sta(x):
+        print ("{}".format(x))        
 
 bart = Student('Bart Simpson', 59)
 lisa = Student('Lisa Simpson', 87)
 
 bart.print_score()
 lisa.print_score()
+
+bart.print_cls()
+Student.print_cls()
+
+bart.print_sta(3)
+Student.print_sta(4)
